1b2c54d0bda32da5ca6d18f10d9ee32567847bb0
Om Nadar <omsankarnadar@gmail.com>
2026-02-09 12:45:25 +0530

    Added Division function in main.py

--- main.py
+++ main.py
@@ -7,17 +7,10 @@ import multiplication as m
 # Caliing functions
 print("Addition Result: ", a.add(5,4))
 print("Subtraction Result: ", s.sub(5,4))
-<<<<<<< HEAD
-print("Multiplication Result: ", m.mul(5,4))
-=======
 print("Multiplication Result: ", m.mul(5,4))
 
 # Here call division function
-<<<<<<< HEAD
->>>>>>> origin/main
-=======
 def div(a,b):
   return a/b
 
 print("Division Result: ", div(20,5))
->>>>>>> origin
