5571ff099dd6a8e3b3ee05368656d724ace15444
Om Nadar <omsankarnadar@gmail.com>
2026-02-09 12:24:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import addition as a
 import subtraction as s
 import multiplication as m
 
+# Caliing functions
 print("Addition Result: ", a.add(5,4))
 print("Subtraction Result: ", s.sub(5,4))
 print("Multiplication Result: ", m.mul(5,4))
